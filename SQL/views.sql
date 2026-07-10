f78a3c15b7d4a4df3675e4f17b88c2e6c6569d1f
Pritkasundra <pritkasundra229@gmail.com>
2026-07-10 19:41:58 +0530

    Add views

---
+++ SQL/views.sql
@@ -0,0 +1,77 @@
+-- 1. Subscriber SIM & Plan Details
+CREATE VIEW subscriber_sim_plan_details AS
+SELECT
+    s.subscriber_id,
+    s.name AS subscriber_name,
+    sc.sim_id,
+    sc.mobile_no,
+    sc.sim_status,
+    sp.plan_id,
+    sp.plan_name,
+    sp.plan_category,
+    sp.charge AS plan_charge,
+    st.start_date,
+    st.end_date,
+    st.status AS subscription_status
+FROM subscriber s
+JOIN sim_card sc
+    ON s.subscriber_id = sc.subscriber_id
+JOIN subscribe_to st
+    ON sc.sim_id = st.sim_id
+JOIN service_plan sp
+    ON st.plan_id = sp.plan_id;
+
+
+-- 2. Tower Activity Summary
+CREATE VIEW tower_activity_summary AS
+SELECT
+    ct.tower_id,
+    ct.location,
+    ct.coverage_area,
+    ct.tower_status,
+    COUNT(tc.sim_id) AS total_connections,
+    COUNT(DISTINCT tc.sim_id) AS unique_sims,
+    AVG(tc.signal_strength) AS avg_signal_strength
+FROM cell_tower ct
+LEFT JOIN tower_connection tc
+    ON ct.tower_id = tc.tower_id
+GROUP BY
+    ct.tower_id,
+    ct.location,
+    ct.coverage_area,
+    ct.tower_status;
+
+
+-- 3. Pending Complaints
+CREATE VIEW pending_complaints AS
+SELECT
+    c.complaint_id,
+    c.complaint_date,
+    c.complaint_type,
+    c.description,
+    c.complaint_status,
+    s.subscriber_id,
+    s.name AS subscriber_name
+FROM complaint c
+JOIN subscriber s
+    ON c.subscriber_id = s.subscriber_id
+WHERE c.Resolution_date IS NULL;   -- FIX: added missing semicolon
+
+
+-- 4. Pending Bills
+CREATE VIEW pending_bills AS
+SELECT
+    b.bill_id,
+    sc.sim_id,
+    sc.mobile_no,
+    b.bill_date,
+    b.due_date,
+    b.call_charge,
+    b.sms_charge,
+    b.data_charge,
+    b.gst,
+    b.bill_status
+FROM bill b
+JOIN sim_card sc
+    ON b.sim_id = sc.sim_id
+WHERE b.bill_status = 'Pending';
